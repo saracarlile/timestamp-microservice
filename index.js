3e5bbd82cc89bd698e6c45ff566dc7b8fd115bd3
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-01-09 13:02:45 -0600

    working with params

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 var path = require('path');
+
+function writeME(parms){
+   console.log(parms);
+}
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.use('/[a-zA-Z0-9., ]+/', function(req, res) {
+   res.end(writeME(req.params));
+});
+
+
+
 
 app.listen(process.env.PORT || 8080);
 
