c0d9631d42bc6b9a3d57ed310fb778b9fdac95a4
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-01-09 16:19:16 -0600

    updates to project

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var moment = require('moment');
 var app = express();
 var path = require('path');
 
-function writeME(parms){
-   console.log(parms);
-}
+
 
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+//app.get('/', function(req, res) {
+  //  res.sendFile(path.join(__dirname + '/index.html'));
+//});
+
+app.use(express.static(__dirname + "/public"));
 
 app.use('/[a-zA-Z0-9., ]+/', function(req, res) {
    res.end(writeME(req.params));
