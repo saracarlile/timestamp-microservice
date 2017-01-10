09a1ac4e61c4990660efe5f6a3edc782982084e0
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-01-10 10:17:18 -0600

    add css

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var moment = require('moment');
 var app = express();
-moment().format();
-
-function checkNaturalDate(input) {
-  var str = input;
-  var re = /(January|February|March|April|May|June?|July|August|September|October|November|December)\s(\d\d?).+?(\d\d\d\d)/;
-  var result = str.match(re);
-  return result;
-}
 
 
-// viewed at http://localhost:8080
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public/css/'));
+//server index.html out of /public
 
 app.get('/:date', function (req, res) {
   var output = '';
@@ -22,18 +15,18 @@ app.get('/:date', function (req, res) {
     var naturalDate = moment.unix(input).format('LL');
     output = { "unix": input, "natural": naturalDate }
   }
-  else if (checkNaturalDate(input)) {
+  else if (moment(input).isValid()) {
     var unix = moment(input).unix();
     output = { "unix": unix, "natural": input }
   }
-  else{
+  else {
     output = { "unix": null, "natural": null }
   }
-
 
   res.json(output)
 });
 
 app.listen(process.env.PORT || 8080);
 
-console.log("Your server is now running at 8080");
+console.log("server now running - port 8080 on localhost");
+// viewed at http://localhost:8080
